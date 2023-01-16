7a37ea3180105ded3d55d2aa2c46c04608f2fc22
Dohuy5360 <loldh321@gmail.com>
2023-01-16 14:07:19 +0700

    Fix __dirname error

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
 
+//*[local] D:\Javascript\web\Hybrid\index.js
+const __filename = fileURLToPath(import.meta.url);
+//*[local] D:\Javascript\web\Hybrid
+const __dirname = dirname(__filename);
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 app.get("/", (req, res) => {
 	res.send("Hell");
 });
