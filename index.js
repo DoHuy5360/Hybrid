070919b058dda5b12574465353449e142c08bfe1
Dohuy5360 <loldh321@gmail.com>
2023-01-16 15:48:41 +0700

    Config test view, 404 page redirection

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ import express from "express";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import path from "path";
+import fs from "fs";
 
 //*[local] D:\Javascript\web\Hybrid\index.js
 const __filename = fileURLToPath(import.meta.url);
 //*[local] D:\Javascript\web\Hybrid
 const __dirname = dirname(__filename);
 const app = express();
-
-app.set("views", path.join(__dirname, "views"));
+app.use(express.static("src/public"));
+app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "pug");
 app.get("/", (req, res) => {
-	res.send("Hell");
+	res.render("home/navigate");
+});
+app.get("/test/:branch", (req, res) => {
+	const testPath = __dirname + "/src/views/test";
+	const validFileNameList = fs.readdirSync(testPath).map((file) => file.split(".")[0]);
+	const branch = req.params.branch;
+	if (validFileNameList.includes(branch)) {
+		res.render(`test/${branch}`);
+	} else {
+		res.render("errors/404");
+	}
+});
+app.get("*", (req, res) => {
+	res.render("errors/404");
 });
 
 const PORT = process.env.PORT || 4000;
