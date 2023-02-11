721f0733665af78de9f56bc960f26bb5ab4bc233
Dohuy5360 <loldh321@gmail.com>
2023-02-12 05:35:28 +0700

    Close project

--- src/public/assets/js/test/tray.js
+++ src/public/assets/js/test/tray.js
@@ -19,31 +19,36 @@ tray_entity.control((on) => {
 		tray_entity.remove_class("dragoverTrigger");
 	});
 });
-const components_tray = document.querySelector("#components_tray");
-
-const components_tray_entity = new INTERACTIVE(components_tray);
-components_tray_entity.can_move(false);
-components_tray_entity.control((on) => {
-	on.dragover(() => {
-		components_tray_entity.add_class("dragoverTrigger");
-	});
-	on.dragleave(() => {
-		components_tray_entity.remove_class("dragoverTrigger");
-	});
-	on.drop((thisTray) => {
-		thisTray.appendChild(getTemporary());
-		components_tray_entity.remove_class("dragoverTrigger");
+const components_tray = document.querySelectorAll(".components_tray");
+components_tray.forEach((tray) => {
+	const components_tray_entity = new INTERACTIVE(tray);
+	components_tray_entity.can_move(false);
+	components_tray_entity.control((on) => {
+		on.dragover(() => {
+			components_tray_entity.add_class("dragoverTrigger");
+		});
+		on.dragleave(() => {
+			components_tray_entity.remove_class("dragoverTrigger");
+		});
+		on.drop((thisTray) => {
+			thisTray.appendChild(getTemporary());
+			components_tray_entity.remove_class("dragoverTrigger");
+		});
 	});
 });
 async function getComponents() {
-	const codes = ["header1", "youtube_header"];
+	const wrap = document.createElement("div");
+	wrap.setAttribute("class", "flex-col components_tray over-scroll-y g-1px no-display");
+	wrap.setAttribute("data-field", "header");
+	const codes = ["header1", "youtube_header", "facebook_header"];
 	for (let idx = 0; idx < codes.length; idx++) {
 		const code = codes[idx];
 		const response = await fetch(`http://localhost:4000/cake/${code}`).then((res) => res);
 		if (response.ok) {
-			components_tray.insertAdjacentHTML("beforeend", await response.text());
+			wrap.insertAdjacentHTML("beforeend", await response.text());
 		}
 	}
+	components_tray.appendChild(wrap);
 	setMicrowave();
 }
 getComponents();
@@ -60,3 +65,15 @@ function setMicrowave() {
 		});
 	});
 }
+const type_select = document.querySelectorAll(".typeSelect");
+let first_field = document.querySelector("[data-field]");
+console.log(first_field);
+type_select.forEach((type) => {
+	type.addEventListener("click", (e) => {
+		first_field.classList.add("no-display");
+		const data_select = type.getAttribute("data-select");
+		const field_selected = document.querySelector(`[data-field="${data_select}"]`);
+		field_selected.classList.remove("no-display");
+		first_field = field_selected;
+	});
+});
