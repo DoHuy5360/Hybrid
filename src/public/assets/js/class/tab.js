6ee6f4181d263bc83e195d0277edd6e4487c8062
Dohuy5360 <loldh321@gmail.com>
2023-02-21 23:10:00 +0700

    Selecting tab & table when clicked corresponse file tree

--- src/public/assets/js/class/tab.js
+++ src/public/assets/js/class/tab.js
@@ -2,21 +2,21 @@ import DOM_FACTORY from "./dom_factory.js";
 import LOGIC from "./logic.js";
 import { file_selected } from "./file.js";
 export const tab_logic = new LOGIC();
-const table_logic = new LOGIC();
-let tab_selected = {
-	dom: undefined,
+export const table_logic = new LOGIC();
+export let tab_selected = {
+	node: undefined,
 };
 class TAB extends DOM_FACTORY {
 	constructor() {
 		super();
-		this.close_sign = this.convert_to_dom('<i class="fa-solid fa-xmark"></i>');
+		this.close_sign = this.convert_to_dom('<div class="close-tab"><i class="fa-solid fa-xmark"></i></div>');
 	}
 	create_tab(file_reference, content_table) {
 		const file_tab = this.create({ type: "div", attribute: { class: "tab selected" } });
 		const file_name = this.create({ type: "div", text: file_reference.name });
 		this.close_sign.addEventListener("click", (e) => {
 			e.stopImmediatePropagation();
-			if (tab_selected.dom === file_tab) {
+			if (tab_selected.node === file_tab) {
 				const last_tab = [...document.querySelectorAll(".tab")].at(-1);
 				if (last_tab) {
 					const beside_tab = tab_logic.previous.previousSibling || tab_logic.previous.nextSibling;
@@ -24,7 +24,7 @@ class TAB extends DOM_FACTORY {
 					if (beside_tab && beside_table) {
 						tab_logic.replace_handle({ domObject: beside_tab, className: "selected" });
 						table_logic.replace_handle({ domObject: beside_table, className: "selected" });
-						tab_selected.dom = beside_tab;
+						tab_selected.node = beside_tab;
 					}
 				}
 			}
@@ -33,12 +33,13 @@ class TAB extends DOM_FACTORY {
 			file_reference.open = false;
 		});
 		file_tab.addEventListener("click", (e) => {
-			if (tab_selected.dom !== file_tab) {
+			console.log("tab clicked");
+			if (tab_selected.node !== file_tab) {
 				tab_logic.replace_handle({ domObject: file_tab, className: "selected" });
 				table_logic.replace_handle({ domObject: content_table, className: "selected" });
 				file_selected.node = file_reference.identify;
 				file_selected.attrs = file_reference.attribute;
-				tab_selected.dom = file_tab;
+				tab_selected.node = file_tab;
 			}
 		});
 		file_tab.appendChild(file_name);
